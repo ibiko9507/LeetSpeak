1e625577b54298d49609a01083bbf5ab4582e675
İbrahim <ibiko.9507@gmail.com>
2023-04-16 17:33:54 +0300

    Projeye Ui katmanı eklendi Layout sayfasına sliding nav yazıldı

---
+++ LeetSpeak.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LeetSpeak.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
